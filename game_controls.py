c4a329dcf1c1cc72d2e8f336d66398c4e9eedcb6
Claire Ingrey <cingrey@sandiego.edu>
2022-02-22 20:08:00 -0800

    land shark

--- game_controls.py
+++ game_controls.py
@@ -179,9 +179,9 @@ def finger_tracking():
         numFingers = 0
         majorFeatures = []
 
-        if len(processedFrame) != 0:                  # loop through number of hands
-            for item in processedFrame:
-                for id, lm in enumerate(item.landmark):
+        if processedFrame.muli_hand_landmarks != 0:                  # loop through number of hands
+            for landmarks in processedFrame.muli_hand_landmarks:
+                for id, lm in enumerate(landmarks.landmark):
                     height = frame.shape[0]
                     width = frame.shape[1]
                     x = lm.x*width
